78354a9100f92491b67de800744a3a769aa8c624
Justin Davis <jtdavis777@gmail.com>
2017-09-02 20:09:54 -0400

    added proper headline saving and updating

--- headlines.py
+++ headlines.py
@@ -3,6 +3,16 @@ from SolveHeadlineManually import *
 from WordTree import *
 
 
+# print out current headlines
+def printHeadlines(headlines):
+    C1, C2, C3, C4, C5 = headlines
+    print('Headlines:\n{}\n{}\n{}\n{}\n{}\n'.format(C1[0],
+                                                    C2[0],
+                                                    C3[0],
+                                                    C4[0],
+                                                    C5[0]))
+
+
 # get a headline choice
 def pickHeadline(headlines):
     selected = None
@@ -10,7 +20,6 @@ def pickHeadline(headlines):
     C1, C2, C3, C4, C5 = headlines
     while(error):
         # print out all the headlines
-        print('{}\n{}\n{}\n{}\n{}\n'.format(C1[0], C2[0], C3[0], C4[0], C5[0]))
         try:
             selected = eval('C' + input('Please pick a headline [1-5]: '))
             error = False
@@ -29,19 +38,21 @@ def initialize():
                           in enumerate(open(argv[2], 'r'))
                           if i < 5]  # only gets the first 5 lines of clues
     print('Done!')
-    return (tree, ((C1, []), (C2, []), (C3, []), (C4, []), (C5, [])))
+    return [tree, [(C1, []), (C2, []), (C3, []), (C4, []), (C5, [])]]
 
 
 # where shit happens
 def main():
     tree, headlines = initialize()
     while True:
+        printHeadlines(headlines)
         try:
             selection = int(input('What would you like to do?\n' +
                                   '1.\tSolve a Headline Manually\n' +
                                   '2.\tView Discovered Chains\n'
                                   '3.\tBuild a Matrix\n' +
-                                  '4.\tReset a Headline and its Chains\n'))
+                                  '4.\tReset a Headline and its Chains\n\n' +
+                                  'Selection: '))
         except ValueError:
             print('Invalid selection, try again')
             continue
@@ -51,6 +62,10 @@ def main():
             solved = solveHeadline(tree, line[0])
             if solved is not None:
                 # successfully solved a headline, update the headlines
+                for i, headline in enumerate(headlines):
+                    if headline[0] == line[0]:
+                        headlines[i] = solved
+                        break
                 continue
         # View Discovered chains
         if selection == 2:
